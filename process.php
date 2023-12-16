8d950617da4877d9630e33affff77fe63d66e4f0
Jesryl-Baguio <dev.jesryl.baguio@gmail.com>
2023-12-17 03:12:26 +0800

    updated contact us form

--- process.php
+++ process.php
@@ -1,9 +1,4 @@
-
-
-
 <?php
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -12,15 +7,10 @@ require './PHPMailer/src/PHPMailer.php';
 require './PHPMailer/src/SMTP.php';
 
 if(isset($_POST['send'])) {
-    $name = $_POST['form-name'];
-    $email = $_POST['form-email'];
-    $subject = $_POST['form-subject'];
-    $message = $_POST['form-message'];
-
-    //Load Composer's autoloader
-    //require 'vendor/autoload.php';
-
-    //Create an instance; passing `true` enables exceptions
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
     $mail = new PHPMailer(true);
 
 try {
@@ -32,27 +22,19 @@ try {
     $mail->Username   = 'jesrylbaguio@gmail.com';              //SMTP username
     $mail->Password   = 'uhaxnwqvzkvsphqf';                    //SMTP password
     $mail->SMTPSecure = 'tls';                                 //Enable implicit TLS encryption
-    $mail->Port       = 587;                                   //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Port       = 587;                                   //TCP port to connect to; use 587 if you have set `SMTPSecure = 
 
     //Recipients
     $mail->setFrom($email, $name);
     $mail->addAddress('jesrylbaguio@gmail.com', 'Jesryl Baguio');     //Add a recipient                     
-    //$mail->addReplyTo('info@example.com', 'Information');
-    //$mail->addCC('cc@example.com');
-    //$mail->addBCC('bcc@example.com');
-
-    //Attachments
-    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = $subject;
-    $mail->Body    = $message;
-    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
     echo 'Message has been sent';
+    //exit();
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
