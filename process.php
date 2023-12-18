0525015f0045c082cd5fb02103e7e34ba9372548
Jesryl-Baguio <dev.jesryl.baguio@gmail.com>
2023-12-18 17:06:54 +0800

    updated contact form

--- process.php
+++ process.php
@@ -1,6 +1,6 @@
 <?php
 // Configure your Subject Prefix and Recipient here
-$subjectPrefix = 'New Email Sent';
+$subjectPrefix = '[Contact via website]';
 $emailTo       = 'jesrylbaguio@gmail.com';
 $errors = array(); // array to hold validation errors
 $data   = array(); // array to pass back data
